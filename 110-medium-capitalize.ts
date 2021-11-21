a80cafd2353b5400c6db1de4fde536ec6caa1e6a
YuziM <mayuz.bubupu@gmail.com>
2021-11-22 00:00:04 +1300

    First commit

---
+++ 110-medium-capitalize.ts
@@ -0,0 +1,39 @@
+/*
+  110 - Capitalize
+  -------
+  by Anthony Fu (@antfu) #medium #template-literal
+  
+  ### Question
+  
+  Implement `Capitalize<T>` which converts the first letter of a string to uppercase and leave the rest as-is.
+  
+  For example
+  
+  ```ts
+  type capitalized = Capitalize<'hello world'> // expected to be 'Hello world'
+  ```
+  
+  > View on GitHub: https://tsch.js.org/110
+*/
+
+/* _____________ Your Code Here _____________ */
+type Capitalize<S extends string> = S extends `${infer P1}${infer P2}`
+  ? `${Uppercase<P1>}${P2}`
+  : S;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Capitalize<"foobar">, "Foobar">>,
+  Expect<Equal<Capitalize<"FOOBAR">, "FOOBAR">>,
+  Expect<Equal<Capitalize<"foo bar">, "Foo bar">>,
+  Expect<Equal<Capitalize<"">, "">>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/110/answer
+  > View solutions: https://tsch.js.org/110/solutions
+  > More Challenges: https://tsch.js.org
+*/
---
+++ README.md
@@ -0,0 +1,4 @@
+# TypeScript challenge 
+
+The workout of typescript challenges from this repo [type-challenges](https://github.com/type-challenges/type-challenges);
+
