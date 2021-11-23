24ff7869af225e3f12c00000f73ffae9cfea9595
YuziM <mayuz.bubupu@gmail.com>
2021-11-23 23:30:47 +1300

    merge & camelcase

---
+++ 599-medium-merge.ts
@@ -0,0 +1,55 @@
+/*
+  599 - Merge
+  -------
+  by ZYSzys (@ZYSzys) #medium #object
+  
+  ### Question
+  
+  Merge two types into a new type. Keys of the second type overrides keys of the first type.
+  
+  > View on GitHub: https://tsch.js.org/599
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Merge<F extends Record<string, any>, S extends Record<string, any>> = {
+  [P in keyof F | keyof S]: P extends keyof S
+    ? S[P]
+    : P extends keyof F
+    ? F[P]
+    : never;
+};
+
+type A = Merge<Foo, Bar>;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from "@type-challenges/utils";
+
+type Foo = {
+  a: number;
+  b: string;
+};
+type Bar = {
+  b: number;
+  c: boolean;
+};
+
+type cases = [
+  Expect<
+    Equal<
+      Merge<Foo, Bar>,
+      {
+        a: number;
+        b: number;
+        c: boolean;
+      }
+    >
+  >
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/599/answer
+  > View solutions: https://tsch.js.org/599/solutions
+  > More Challenges: https://tsch.js.org
+*/
